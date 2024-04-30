0d2a268965a6adf61f02f1d00f58d32185415dfa
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-04-30 13:50:11 +0300

    Update

---
+++ ITDesk.WebAPI/Models/ChatRoom.cs
@@ -0,0 +1,11 @@
+﻿namespace ITDesk.WebAPI.Models;
+
+public sealed class ChatRoom
+{
+    public ChatRoom()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+
+}
---
+++ ITDesk.WebAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace ITDesk.WebAPI.Models;
+
+public sealed class User
+{
+    public User()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = new byte[64];
+    public byte[] PasswordSalt { get; set; } = new byte[128];
+}
